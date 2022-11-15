f147b6537b92d1c87dd12c95b5a767912ffe7516
gloooow <stefanncula@gmail.com>
2022-11-15 20:59:42 +0100

    added money_management app

--- epplow/epplow/settings.py
+++ epplow/epplow/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&pkyq&(35xhccrm*%93$^)13mlim88z)qi5mplpwr@mmlifo$'
+SECRET_KEY = 'django-insecure-690w#%pe+9+p)@pp%7@of=sw8=(!g33n32d*jb#9f=much@0zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
