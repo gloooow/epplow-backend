8c10480c58eff4ab9cb68bd6a8c2cfb052b726d5
gloooow <stefanncula@gmail.com>
2022-11-15 22:30:42 +0100

    added accounts

--- epplow/money_management/admin.py
+++ epplow/money_management/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Category, Item
+from .models import Category, Item, Account, AccountCode
 
 admin.site.register(Category)
 admin.site.register(Item)
-
+admin.site.register(Account)
+admin.site.register(AccountCode)
--- epplow/money_management/models.py
+++ epplow/money_management/models.py
@@ -5,7 +5,11 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
-
+    def __str__(self):
+        return self.name
+class AccountCode(models.Model):
+    name = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
 
@@ -21,6 +25,22 @@ class Item(models.Model):
     tax = models.FloatField(default=0)
     type = models.CharField(max_length=10, choices=Type.choices, default=Type.EXPENSE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Account(models.Model):
+    class Currency(models.TextChoices):
+        RON = 'RON'
+        PLN = 'PLN'
+        EUR = 'EUR'
+        USD = 'USD'
+
+    name = models.CharField(max_length=100)
+    code = models.ForeignKey(AccountCode, on_delete=models.CASCADE, null=True, blank=True)
+    balance = models.FloatField(default=0)
+    currency = models.CharField(max_length=10, choices=Currency.choices, default=Currency.RON)
+
 
     def __str__(self):
         return self.name
